f7eb5cd0f112f22e28f9271603c281e7da9f1d48
Vicky <kendrevitthal225@gmail.com>
2025-08-20 23:36:35 +0530

    some minor testin done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -49,6 +49,10 @@ export default function ProfilePage(){
                 className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >Get User Details</button>
 
+                <button
+                onClick={getUserDetails}
+                className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >Get User Details</button>
 
             </div>
     )
